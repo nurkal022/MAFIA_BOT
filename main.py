e4a5dd7f72b2ce5ea6768b22498822c93b21e53a
nurkal022 <nurkal022@gmail.com>
2024-08-04 17:05:00 +0500

    efe

--- main.py
+++ main.py
@@ -3,24 +3,8 @@ from aiogram import Bot, Dispatcher
 from app.config import TOKEN
 from app.handlers.commands import set_private_commands, set_group_commands
 from app.handlers import game,profile,start,callbacks,commands,admin
-# 44
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#kerngjernognpih
 async def main():
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
@@ -37,10 +21,7 @@ async def main():
     
     # Устанавливаем команды для группового чата
     await set_group_commands(bot)
-    # bot_info = await bot.get_me()
-    # bot_username = bot_info.username
-    # print(bot_username)
-    # Устанавливаем команды для admin чата
+
     # await set_admin_commands(bot)
     
     await dp.start_polling(bot)
